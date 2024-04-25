1602dd5a5c67f368bcd8aa3a33b64856fde0cdef
GjozinskiBodan <b.gjozinski@ins.com.mk>
2024-04-25 12:26:01 +0200

    feat: add detection run on click with hard coded model

--- MainApp.BL/Mappers/UserManagementProfileBL.cs
+++ MainApp.BL/Mappers/UserManagementProfileBL.cs
@@ -9,27 +9,21 @@ namespace MainApp.BL.Mappers
     {
         public UserManagementProfileBL()
         {
-            CreateMap<UserManagementDTO, ApplicationUser>()
-            .ForMember(dest => dest.Id, opt => opt.Ignore())
-            .ForMember(dest => dest.NormalizedUserName, opt => opt.Ignore())
-            .ForMember(dest => dest.NormalizedEmail, opt => opt.Ignore())
-            .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
-            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
-            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
-            .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
-            .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
-            .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
-            .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => src.IsActive ?? false));
+            CreateMap<UserDTO, ApplicationUser>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.NormalizedUserName, opt => opt.Ignore())
+                .ForMember(dest => dest.NormalizedEmail, opt => opt.Ignore())
+                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
+                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
+                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
+                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => src.IsActive ?? false));
 
-            CreateMap<ApplicationUser, UserManagementDTO>()
-            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-            .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
-            .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
-            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
-            .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
-            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
-            .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => src.IsActive));
+            CreateMap<UserManagementDTO, ApplicationUser>();
 
+            CreateMap<ApplicationUser, UserManagementDTO>();
 
             CreateMap<string, IdentityUserRole<string>>()
             .ForMember(dest => dest.UserId, opt => opt.Ignore())
--- MainApp.MVC/Program.cs
+++ MainApp.MVC/Program.cs
@@ -40,6 +40,10 @@ using DAL.Interfaces.Repositories.MapConfigurationRepositories;
 using DAL.Repositories.MapConfigurationRepositories;
 using MainApp.BL.Interfaces.Services.MapConfigurationServices;
 using MainApp.BL.Services.MapConfigurationServices;
+using MainApp.BL.Services.DetectionServices;
+using DAL.Repositories.DetectionRepositories;
+using DAL.Interfaces.Repositories.DetectionRepositories;
+using MainApp.BL.Interfaces.Services.DetectionServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -176,6 +180,8 @@ builder.Services.TryAddScoped<IMapLayersConfigurationService, MapLayersConfigura
 builder.Services.TryAddScoped<IMapLayerGroupsConfigurationRepository, MapLayerGroupsConfigurationRepository>();
 builder.Services.TryAddScoped<IMapLayerGroupsConfigurationService, MapLayerGroupsConfigurationService>();
 
+builder.Services.TryAddScoped<IDetectionRunsRepository, DetectionRunsRepository>();
+builder.Services.TryAddScoped<IDetectionRunService, DetectionRunService>();
 
 // TODO: look over
 services.AddAutoMapper(typeof(Program).Assembly, typeof(UserManagementProfileBL).Assembly);
@@ -183,6 +189,9 @@ services.AddAutoMapper(typeof(Program).Assembly, typeof(UserManagementProfile).A
 services.AddAutoMapper(typeof(Program).Assembly, typeof(DatasetProfileBL).Assembly);
 services.AddAutoMapper(typeof(Program).Assembly, typeof(DatasetProfile).Assembly);
 services.AddAutoMapper(typeof(Program).Assembly, typeof(MapConfigurationProfileBL).Assembly);
+services.AddAutoMapper(typeof(Program).Assembly, typeof(DetectionProfileBL).Assembly);
+//services.AddAutoMapper(typeof(Program).Assembly, typeof(MainApp.BL.Infrastructure.RegisterServices).Assembly);
+
 
 //services.RegisterAuditNet();
 Audit.Core.Configuration.Setup()
