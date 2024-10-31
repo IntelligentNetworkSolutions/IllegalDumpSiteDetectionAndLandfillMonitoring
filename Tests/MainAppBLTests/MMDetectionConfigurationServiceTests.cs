47ab754c00976b3fbf99db3adb015896b7592b01
GjozinskiBodan <b.gjozinski@ins.com.mk>
2024-10-31 03:48:33 +0100

    tests: mmdetection service

--- Tests/MainAppBLTests/MMDetectionConfigurationServiceTests.cs
+++ Tests/MainAppBLTests/MMDetectionConfigurationServiceTests.cs
@@ -1,6 +1,9 @@
 ﻿using System.Linq.Expressions;
+using MainApp.BL.Interfaces.Services;
+using MainApp.BL.Services.TrainingServices;
 using Microsoft.Extensions.Configuration;
 using Moq;
+using SD;
 using SD.Helpers;
 
 namespace Tests.MainAppBLTests
@@ -41,162 +44,7 @@ namespace Tests.MainAppBLTests
             mockChildSection.Setup(s => s.Value).Returns(value);
             parentSection.Setup(s => s[key]).Returns(value);
         }
-        /*
-        [Fact]
-        public void GetConfiguration_ReturnsCorrectConfiguration()
-        {
-            var config = _service.GetConfiguration();
-            Assert.NotNull(config);
-            Assert.Equal("C:\\path\\to\\conda", config.Base.CondaExeAbsPath);
-            Assert.Equal("C:\\path\\to\\root", config.Base.RootDirAbsPath);
-            Assert.Equal("scripts", config.Base.ScriptsDirRelPath);
-        }
 
-        [Fact]
-        public void GetRootDirAbsPath_ReturnsCorrectPath()
-        {
-            var path = _service.GetRootDirAbsPath();
-            Assert.Equal("C:/path/to/root", path);
-        }
-
-        [Fact]
-        public void GetCondaExeAbsPath_ReturnsCorrectPath()
-        {
-            var path = _service.GetCondaExeAbsPath();
-            Assert.Equal("C:/path/to/conda", path);
-        }
-
-        [Fact]
-        public void GetScriptsDirAbsPath_ReturnsCorrectPath()
-        {
-            var path = _service.GetScriptsDirAbsPath();
-            Assert.Equal("C:/path/to/root/scripts", path);
-        }
-
-        [Fact]
-        public void GetBackboneCheckpointAbsPath_ReturnsCorrectPath()
-        {
-            var path = _service.GetBackboneCheckpointAbsPath();
-            Assert.Equal("C:/path/to/backbone", path);
-        }
-
-        [Fact]
-        public void GetDatasetsDirAbsPath_ReturnsCorrectPath()
-        {
-            var path = _service.GetDatasetsDirAbsPath();
-            Assert.Equal("C:/path/to/root/datasets", path);
-        }
-
-        [Fact]
-        public void GetTrainingRunDatasetDirAbsPath_ReturnsCorrectPath()
-        {
-            var guid = Guid.NewGuid();
-            var path = _service.GetTrainingRunDatasetDirAbsPath(guid);
-            Assert.Equal($"C:/path/to/root/datasets/{guid}", path);
-        }
-
-        [Fact]
-        public void GetConfigsDirAbsPath_ReturnsCorrectPath()
-        {
-            var path = _service.GetConfigsDirAbsPath();
-            Assert.Equal("C:/path/to/root/configs", path);
-        }
-
-        [Fact]
-        public void GetTrainingRunConfigDirAbsPathByRunId_ReturnsCorrectPath()
-        {
-            var guid = Guid.NewGuid();
-            var path = _service.GetTrainingRunConfigDirAbsPathByRunId(guid);
-            Assert.Equal($"C:/path/to/root/configs/{guid}", path);
-        }
-
-        [Fact]
-        public void GetTrainingRunConfigFileAbsPathByRunId_ReturnsCorrectPath()
-        {
-            var guid = Guid.NewGuid();
-            var path = _service.GetTrainingRunConfigFileAbsPathByRunId(guid);
-            Assert.Equal($"C:/path/to/root/configs/{guid}/{guid}.py", path);
-        }
-
-        [Fact]
-        public void GetTrainingRunsBaseOutDirAbsPath_ReturnsCorrectPath()
-        {
-            var path = _service.GetTrainingRunsBaseOutDirAbsPath();
-            Assert.Equal("C:/path/to/root/output", path);
-        }
-
-        [Fact]
-        public void GetTrainingRunOutDirAbsPathByRunId_ReturnsCorrectPath()
-        {
-            var guid = Guid.NewGuid();
-            var path = _service.GetTrainingRunOutDirAbsPathByRunId(guid);
-            Assert.Equal($"C:/path/to/root/output/{guid}", path);
-        }
-
-        [Fact]
-        public void GetTrainingRunResultLogFileAbsPath_ReturnsFailure_WhenDirectoryDoesNotExist()
-        {
-            var guid = Guid.NewGuid();
-            var result = _service.GetTrainingRunResultLogFileAbsPath(guid);
-            Assert.False(result.IsSuccess);
-            Assert.Equal("", result.ErrMsg);
-        }
-
-        [Fact]
-        public void GetTrainedModelConfigFileAbsPath_ReturnsFailure_WhenDirectoryDoesNotExist()
-        {
-            var guid = Guid.NewGuid();
-            var result = _service.GetTrainedModelConfigFileAbsPath(guid);
-            Assert.False(result.IsSuccess);
-            Assert.Equal("Failed to Access Training Run Output Directory, No Permissions or Directory does Not Exist", result.ErrMsg);
-        }
-
-        [Fact]
-        public void GetTrainedModelBestEpochFileAbsPath_ReturnsFailure_WhenDirectoryDoesNotExist()
-        {
-            var guid = Guid.NewGuid();
-            var result = _service.GetTrainedModelBestEpochFileAbsPath(guid);
-            Assert.False(result.IsSuccess);
-            Assert.Equal("Failed to Access Training Run Output Directory, No Permissions or Directory does Not Exist", result.ErrMsg);
-        }
-
-        [Fact]
-        public void GetTrainingRunCliOutDirAbsPath_ReturnsCorrectPath()
-        {
-            var path = _service.GetTrainingRunCliOutDirAbsPath();
-            Assert.Equal("C:/path/to/training/logs", path);
-        }
-
-        [Fact]
-        public void GetDetectionRunOutputDirAbsPath_ReturnsCorrectPath()
-        {
-            var path = _service.GetDetectionRunOutputDirAbsPath();
-            Assert.Equal("C:/path/to/root/detection_output", path);
-        }
-
-        [Fact]
-        public void GetDetectionRunOutputDirAbsPathByRunId_ReturnsCorrectPath()
-        {
-            var guid = Guid.NewGuid();
-            var path = _service.GetDetectionRunOutputDirAbsPathByRunId(guid);
-            Assert.Equal($"C:/path/to/root/detection_output/{guid}", path);
-        }
-
-        [Fact]
-        public void GetDetectionRunOutputAnnotationsFileAbsPathByRunId_ReturnsCorrectPath()
-        {
-            var guid = Guid.NewGuid();
-            var path = _service.GetDetectionRunOutputAnnotationsFileAbsPathByRunId(guid);
-            Assert.Equal($"C:/path/to/root/detection_output/{guid}/detection_bboxes.json", path);
-        }
-
-        [Fact]
-        public void GetDetectionRunCliOutDirAbsPath_ReturnsCorrectPath()
-        {
-            var path = _service.GetDetectionRunCliOutDirAbsPath();
-            Assert.Equal("C:/path/to/detection/logs", path);
-        }
-        */
         private string ExpectedPath(string windowsPath)
         {
             return Path.DirectorySeparatorChar == '/'
@@ -302,46 +150,63 @@ namespace Tests.MainAppBLTests
             Assert.Equal(ExpectedPath($"C:\\path\\to\\root\\output\\{guid}"), path);
         }
 
-        // TODO: As Integration Test
-        //[Fact]
-        //public void GetTrainingRunResultLogFileAbsPath_ReturnsSuccess_WhenDirectoryExists()
-        //{
-        //    var guid = Guid.NewGuid();
-        //    _mockConfiguration.Setup(x => x["Base:RootDirAbsPath"]).Returns(Path.GetTempPath());
-        //    _mockConfiguration.Object["MMDetectionConfiguration:Base:RootDirAbsPath"] = Path.GetTempPath();
-        //    string runDirPath = _service.GetTrainingRunOutDirAbsPathByRunId(guid);
-        //    string timeRunDirPath = CommonHelper.PathToLinuxRegexSlashReplace(Path.Combine(runDirPath, DateTime.UtcNow.ToString("ffffff")));
-        //    string visDataPath = CommonHelper.PathToLinuxRegexSlashReplace(Path.Combine(timeRunDirPath, "vis_data"));
-        //    string resultsFilePath = CommonHelper.PathToLinuxRegexSlashReplace(Path.Combine(visDataPath, "scalars.json"));
+        [Trait("Category", "Integration")]
+        [Fact]
+        public void GetTrainingRunResultLogFileAbsPath_ReturnsSuccess_WhenDirectoryExists()
+        {
+            var guid = Guid.NewGuid();
+            string rootPath = Path.GetTempPath();
+            var mockService = new Mock<IMMDetectionConfigurationService>();
+            mockService.Setup(x => x.GetTrainingRunOutDirAbsPathByRunId(guid))
+                .Returns(rootPath);
+            string runDirPath = mockService.Object.GetTrainingRunOutDirAbsPathByRunId(guid);
+            string timeRunDirPath =
+                CommonHelper.PathToLinuxRegexSlashReplace(Path.Combine(runDirPath, DateTime.UtcNow.ToString("ffffff")));
+            string visDataPath =
+                CommonHelper.PathToLinuxRegexSlashReplace(Path.Combine(timeRunDirPath, "vis_data"));
+            string resultsFilePath =
+                CommonHelper.PathToLinuxRegexSlashReplace(Path.Combine(visDataPath, "scalars.json"));
 
-        //    try
-        //    {
-        //        if (!Directory.Exists(runDirPath))
-        //            Directory.Delete(runDirPath);
-        //        if (!Directory.Exists(timeRunDirPath))
-        //            Directory.Delete(timeRunDirPath);
-        //        if (!Directory.Exists(visDataPath))
-        //            Directory.Delete(visDataPath);
-        //        if (!File.Exists(resultsFilePath))
-        //            File.Delete(resultsFilePath);
+            Func<Guid, ResultDTO<string>> func = id => id == guid ? ResultDTO<string>.Ok(resultsFilePath) : ResultDTO<string>.Fail("Err");
+            mockService.Setup(x => x.GetTrainingRunResultLogFileAbsPath(guid))
+                .Returns(func);
 
-        //        var result = _service.GetTrainingRunResultLogFileAbsPath(guid);
-        //        Assert.True(result.IsSuccess);
-        //    }
-        //    finally
-        //    {
-        //        if(File.Exists(resultsFilePath))
-        //            File.Delete(resultsFilePath);
-        //        if (Directory.Exists(visDataPath))
-        //            Directory.Delete(visDataPath);
-        //        if (Directory.Exists(timeRunDirPath))
-        //            Directory.Delete(timeRunDirPath);
-        //        if (Directory.Exists(runDirPath))
-        //            Directory.Delete(runDirPath);
 
-        //        _mockConfiguration.Setup(x => x["Base:RootDirAbsPath"]).Returns("C:\\path\\to\\root");
-        //    }
-        //}
+            try
+            {
+                if (!Directory.Exists(runDirPath))
+                    Directory.CreateDirectory(runDirPath);
+                if (!Directory.Exists(timeRunDirPath))
+                    Directory.CreateDirectory(timeRunDirPath);
+                if (!Directory.Exists(visDataPath))
+                    Directory.CreateDirectory(visDataPath);
+
+                string scalarsJson = @"{
+                    ""Training"": {
+                        ""InitialBaseModels"": [
+                            {
+                                ""Name"": ""TestModel"",
+                                ""ConfigDownloadUrl"": ""hdsdttp://test.com/config.py"",
+                                ""ModelFile"": ""httsdsp://test.com/model.pth""
+                            }
+                        ]
+                    }
+                }";
+                File.WriteAllText(resultsFilePath, scalarsJson);
+
+                var result = mockService.Object.GetTrainingRunResultLogFileAbsPath(guid);
+                Assert.True(result.IsSuccess);
+            }
+            finally
+            {
+                if (File.Exists(resultsFilePath))
+                    File.Delete(resultsFilePath);
+                if (Directory.Exists(visDataPath))
+                    Directory.Delete(visDataPath);
+                if (Directory.Exists(timeRunDirPath))
+                    Directory.Delete(timeRunDirPath);
+            }
+        }
 
         [Fact]
         public void GetTrainingRunResultLogFileAbsPath_ReturnsFailure_WhenDirectoryDoesNotExist()
@@ -352,6 +217,42 @@ namespace Tests.MainAppBLTests
             Assert.Equal("", result.ErrMsg);
         }
 
+        [Trait("Category", "Integration")]
+        [Fact]
+        public void GetTrainedModelConfigFileAbsPath_ReturnsSuccess_WhenDirectoryExists()
+        {
+            Guid guid = Guid.NewGuid();
+            string rootPath = Path.Combine(Path.GetTempPath(), $"test_mmdetectionservice_{guid}");
+            Mock<IMMDetectionConfigurationService> mockService = new Mock<IMMDetectionConfigurationService>();
+            mockService.Setup(x => x.GetTrainingRunOutDirAbsPathByRunId(guid))
+                .Returns(rootPath);
+            string runDirPath = mockService.Object.GetTrainingRunOutDirAbsPathByRunId(guid);
+            string configFilePath =
+                CommonHelper.PathToLinuxRegexSlashReplace(Path.Combine(runDirPath, $"{guid}.py"));
+
+            Func<Guid, ResultDTO<string>> func = id => id == guid ? ResultDTO<string>.Ok(configFilePath) : ResultDTO<string>.Fail("Err");
+            mockService.Setup(x => x.GetTrainedModelConfigFileAbsPath(guid))
+                .Returns(func);
+
+
+            try
+            {
+                if (!Directory.Exists(runDirPath))
+                    Directory.CreateDirectory(runDirPath);
+
+                string configPy = @"#### Python Test File ####";
+                File.WriteAllText(configFilePath, configPy);
+
+                var result = mockService.Object.GetTrainedModelConfigFileAbsPath(guid);
+                Assert.True(result.IsSuccess);
+            }
+            finally
+            {
+                if (File.Exists(configFilePath))
+                    File.Delete(configFilePath);
+            }
+        }
+
         [Fact]
         public void GetTrainedModelConfigFileAbsPath_ReturnsFailure_WhenDirectoryDoesNotExist()
         {
@@ -359,6 +260,82 @@ namespace Tests.MainAppBLTests
             var result = _service.GetTrainedModelConfigFileAbsPath(guid);
             Assert.False(result.IsSuccess);
             Assert.Equal("Failed to Access Training Run Output Directory, No Permissions or Directory does Not Exist", result.ErrMsg);
+        }
+
+        [Trait("Category", "Integration")]
+        [Fact]
+        public void GetTrainedModelBestEpochFileAbsPath_NoEpochNumReturnsSuccess_WhenDirectoryExists()
+        {
+            Guid guid = Guid.NewGuid();
+            string rootPath = Path.Combine(Path.GetTempPath(), $"test_mmdetectionservice_{guid}");
+            Mock<IMMDetectionConfigurationService> mockService = new Mock<IMMDetectionConfigurationService>();
+
+            mockService.Setup(x => x.GetTrainingRunOutDirAbsPathByRunId(guid))
+                .Returns(rootPath);
+
+            string runDirPath = mockService.Object.GetTrainingRunOutDirAbsPathByRunId(guid);
+            string configFilePath = CommonHelper.PathToLinuxRegexSlashReplace(Path.Combine(runDirPath, $"epoch_1.pth"));
+
+            Func<Guid, int?, ResultDTO<string>> func = (Guid idPar, int? epochPar = null) =>
+                idPar == guid ? ResultDTO<string>.Ok(configFilePath) : ResultDTO<string>.Fail("Err");
+
+            mockService.Setup(x => x.GetTrainedModelBestEpochFileAbsPath(guid, null))
+                .Returns(() => func(guid, 1));
+
+            try
+            {
+                if (!Directory.Exists(runDirPath))
+                    Directory.CreateDirectory(runDirPath);
+
+                string configPy = @"#### PTH Test File ####";
+                File.WriteAllText(configFilePath, configPy);
+
+                var result = mockService.Object.GetTrainedModelBestEpochFileAbsPath(guid, null);
+                Assert.True(result.IsSuccess);
+            }
+            finally
+            {
+                if (File.Exists(configFilePath))
+                    File.Delete(configFilePath);
+            }
+        }
+
+        [Trait("Category", "Integration")]
+        [Fact]
+        public void GetTrainedModelBestEpochFileAbsPath_WithNumEpochReturnsSuccess_WhenDirectoryExists()
+        {
+            Guid guid = Guid.NewGuid();
+            string rootPath = Path.Combine(Path.GetTempPath(), $"test_mmdetectionservice_{guid}");
+            Mock<IMMDetectionConfigurationService> mockService = new Mock<IMMDetectionConfigurationService>();
+
+            mockService.Setup(x => x.GetTrainingRunOutDirAbsPathByRunId(guid))
+                .Returns(rootPath);
+
+            string runDirPath = mockService.Object.GetTrainingRunOutDirAbsPathByRunId(guid);
+            string configFilePath = CommonHelper.PathToLinuxRegexSlashReplace(Path.Combine(runDirPath, $"epoch_1.pth"));
+
+            Func<Guid, int, ResultDTO<string>> func = (Guid idPar, int epochPar) =>
+                idPar == guid ? ResultDTO<string>.Ok(configFilePath) : ResultDTO<string>.Fail("Err");
+
+            mockService.Setup(x => x.GetTrainedModelBestEpochFileAbsPath(guid, 1))
+                .Returns(() => func(guid, 1));
+
+            try
+            {
+                if (!Directory.Exists(runDirPath))
+                    Directory.CreateDirectory(runDirPath);
+
+                string configPy = @"#### PTH Test File ####";
+                File.WriteAllText(configFilePath, configPy);
+
+                var result = mockService.Object.GetTrainedModelBestEpochFileAbsPath(guid, 1);
+                Assert.True(result.IsSuccess);
+            }
+            finally
+            {
+                if (File.Exists(configFilePath))
+                    File.Delete(configFilePath);
+            }
         }
 
         [Fact]
